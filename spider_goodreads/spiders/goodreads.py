903f4f411687421b031a3c9fa85ab8eac3e118fd
jamesChen <702660780@qq.com>
2019-07-13 15:10:54 +0800

    add

--- spider_goodreads/spiders/goodreads.py
+++ spider_goodreads/spiders/goodreads.py
@@ -53,6 +53,7 @@ class MangoSpider(scrapy.Spider):
         bb=etree.fromstring(details[1]).xpath("./nobr[@class='greyText']/text()")[0].strip().rstrip(")").lstrip("(") if etree.fromstring(details[1]).xpath("./nobr[@class='greyText']/text()") else aa
 
         Rating_details=response.xpath("//span[@id='rating_graph']/script/text()").extract()[0].strip()
+        print "-----"+Rating_details
         renderRatingGraph=re.compile('renderRatingGraph\(.*\[(.*)\],',re.S).search(Rating_details).group(1)
         print renderRatingGraph
         print "\n--------------------图书字段信息-------------------"
