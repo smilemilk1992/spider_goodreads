a72d1b4d8b10c0b7229709aabfa9615b22cbbc69
jamesChen <702660780@qq.com>
2019-07-14 07:06:26 +0800

    add

--- spider_goodreads/spiders/goodreads_book.py
+++ spider_goodreads/spiders/goodreads_book.py
@@ -18,9 +18,9 @@ class MangoSpider(scrapy.Spider):
     '''
     name = "goodreads_book"
     custom_settings = {
-        'CONCURRENT_REQUESTS': 8,  #允许的线程数
+        'CONCURRENT_REQUESTS': 4,  #允许的线程数
         'RETRY_TIMES': 3,  #重试机制
-        'DOWNLOAD_DELAY':2,   #延时（秒）
+        'DOWNLOAD_DELAY':5,   #延时（秒）
         # 'ITEM_PIPELINES': {
         #     "spider_goodreads.pipelines.SpiderPipeline": 200,
         # },
