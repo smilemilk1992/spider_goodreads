1d7540ce21aab7421a2bd1f009c7ead442d285ea
jamesChen <702660780@qq.com>
2019-07-14 01:00:29 +0800

    add

--- spider_goodreads/spiders/goodreads_book.py
+++ spider_goodreads/spiders/goodreads_book.py
@@ -142,7 +142,7 @@ class MangoSpider(scrapy.Spider):
             print "   description    :" + description
             print "--------------------图书字段信息-------------------\n"
         except Exception,e:
-            logging.error("error url="+response.url+" e="+e)
+            logging.error("error url="+response.url)
 
 
 
