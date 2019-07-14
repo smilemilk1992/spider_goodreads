625e41038a01da89cb4341f8e8d84d78688474a9
jamesChen <702660780@qq.com>
2019-07-14 21:13:23 +0800

    add

--- spider_goodreads/spiders/goodreads_library.py
+++ spider_goodreads/spiders/goodreads_library.py
@@ -52,8 +52,9 @@ class LibrarySpider(scrapy.Spider):
     def parse(self, response):
         libsresults=response.xpath("//table[@id='libsresults']//td[@class='name']").extract()
         for lib in libsresults:
-
+            name=re.search('"在此图书馆的目录中查找这个格式 (图书)">(.*?)<',lib).group(1)
+            info = re.search('"geoloc">(.*?)<',lib).group(1)
             # parser = etree.HTMLParser(encoding="utf-8")
             # htmlelement = etree.parse(lib, parser=parser)
             # name = etree.fromstring(lib)
-            print lib
+            print name,info
