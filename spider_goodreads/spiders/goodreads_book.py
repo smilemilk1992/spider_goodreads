5dfa1b849302ceb1542bbbe6d897b3439d642514
jamesChen <702660780@qq.com>
2019-07-14 09:51:12 +0800

    add

--- spider_goodreads/spiders/goodreads_book.py
+++ spider_goodreads/spiders/goodreads_book.py
@@ -21,9 +21,9 @@ class MangoSpider(scrapy.Spider):
         'CONCURRENT_REQUESTS': 4,  #允许的线程数
         'RETRY_TIMES': 3,  #重试机制
         'DOWNLOAD_DELAY':5,   #延时（秒）
-        'ITEM_PIPELINES': {
-            "spider_goodreads.pipelines.pipelines.SpiderGoodreadsPipeline": 200,
-        },
+        # 'ITEM_PIPELINES': {
+        #     "spider_goodreads.pipelines.pipelines.SpiderGoodreadsPipeline": 200,
+        # },
         'DOWNLOADER_MIDDLEWARES': {
             'spider_goodreads.middlewares.RandomUserAgent.RandomUserAgent': 300,
             # 'spider_goodreads.middlewares.random_http_proxy.IpMiddleware': 110, #添加代理ip逻辑
@@ -71,7 +71,7 @@ class MangoSpider(scrapy.Spider):
             ratings=response.xpath("//meta[@itemprop='ratingCount']/@content").extract()[0].strip()
             reviews = response.xpath("//meta[@itemprop='reviewCount']/@content").extract()[0].strip()
             coverPic=response.xpath("//div[@class='noCoverMediumContainer']/img/@src | //img[@id='coverImage']/@src").extract()[0].strip()
-            description=response.xpath("//div[@id='description']/span/text()").extract()[-1].strip() if response.xpath("//div[@id='description']/span/text()").extract() else "None"
+            description=response.xpath("//div[@id='description']/span//text()").extract()[-1].strip() if response.xpath("//div[@id='description']/span/text()").extract() else "None"
             bookFormat=response.xpath("//span[@itemprop='bookFormat']/text()").extract()[0].strip()
             ispage=response.xpath("//span[@itemprop='numberOfPages']/text()").extract()
             if ispage:
@@ -148,56 +148,56 @@ class MangoSpider(scrapy.Spider):
                     genres[a]=b.replace("users","").strip()
 
 
-            item={}
-            item["bookUrl"]=bookUrl
-            item["title"]=title
-            item["authorName"]=",".join(x for x in authorList)
-            item["authorNameUrl"]=",".join(x for x in authorUrlList)
-            item["Illustrator"]=",".join(x for x in Tlluser)
-            item["IllustratorUrl"]=",".join(x for x in Tllist)
-            item["coverPic"]=coverPic
-            item["Rating_details"]=renderRatingGraph
-            item["score"]=score
-            item["ratings"]=ratings
-            item["reviews"]=reviews
-            item["Literary_Awards"]=Literary_Awards
-            item["genres"]=str(genres)
-            item["bookFormat"]=bookFormat
-            item["Published_Time"]=aa
-            item["First_Published_Time"]=bb
-            item["pages"]=pages
-            item["Original_title"]=Original_title
-            item["ISBN"]=ISBN
-            item["ISBN13"]=ISBN13
-            item["Edition_Language"]=Edition_Language
-            item["description"]=description
-            yield item
+            # item={}
+            # item["bookUrl"]=bookUrl
+            # item["title"]=title
+            # item["authorName"]=",".join(x for x in authorList)
+            # item["authorNameUrl"]=",".join(x for x in authorUrlList)
+            # item["Illustrator"]=",".join(x for x in Tlluser)
+            # item["IllustratorUrl"]=",".join(x for x in Tllist)
+            # item["coverPic"]=coverPic
+            # item["Rating_details"]=renderRatingGraph
+            # item["score"]=score
+            # item["ratings"]=ratings
+            # item["reviews"]=reviews
+            # item["Literary_Awards"]=Literary_Awards
+            # item["genres"]=str(genres)
+            # item["bookFormat"]=bookFormat
+            # item["Published_Time"]=aa
+            # item["First_Published_Time"]=bb
+            # item["pages"]=pages
+            # item["Original_title"]=Original_title
+            # item["ISBN"]=ISBN
+            # item["ISBN13"]=ISBN13
+            # item["Edition_Language"]=Edition_Language
+            # item["description"]=description
+            # yield item
 
             #
-            # print "\n--------------------图书字段信息-------------------"
-            # print "   bookUrl    :" + bookUrl
-            # print "   title    :"+title
-            # print "   authorName    :"+",".join(x for x in authorList)
-            # print "   authorNameUrl    :"+",".join(x for x in authorUrlList)
-            # print "   Illustrator   :" + ",".join(x for x in Tlluser)
-            # print "   IllustratorUrl   :"+",".join(x for x in Tllist)
-            # print "   coverPic    :" + coverPic
-            # print "   Rating details    :" + renderRatingGraph
-            # print "   score    :" + score
-            # print "   ratings    :" + ratings
-            # print "   reviews    :" + reviews
-            # print "   genres     :"+str(genres)
-            # print "   bookFormat    :" + bookFormat
-            # print "   Published_Time    :" + aa
-            # print "   First_Published_Time    :" + bb
-            # print "   pages    :" + pages
-            # print "   Original_title    :" + Original_title
-            # print "   Literary_Awards   :"+Literary_Awards
-            # print "   ISBN    :" + ISBN
-            # print "   ISBN13    :" + ISBN13
-            # print "   Edition_Language    :" + Edition_Language
-            # print "   description    :" + description
-            # print "--------------------图书字段信息-------------------\n"
+            print "\n--------------------图书字段信息-------------------"
+            print "   bookUrl    :" + bookUrl
+            print "   title    :"+title
+            print "   authorName    :"+",".join(x for x in authorList)
+            print "   authorNameUrl    :"+",".join(x for x in authorUrlList)
+            print "   Illustrator   :" + ",".join(x for x in Tlluser)
+            print "   IllustratorUrl   :"+",".join(x for x in Tllist)
+            print "   coverPic    :" + coverPic
+            print "   Rating details    :" + renderRatingGraph
+            print "   score    :" + score
+            print "   ratings    :" + ratings
+            print "   reviews    :" + reviews
+            print "   genres     :"+str(genres)
+            print "   bookFormat    :" + bookFormat
+            print "   Published_Time    :" + aa
+            print "   First_Published_Time    :" + bb
+            print "   pages    :" + pages
+            print "   Original_title    :" + Original_title
+            print "   Literary_Awards   :"+Literary_Awards
+            print "   ISBN    :" + ISBN
+            print "   ISBN13    :" + ISBN13
+            print "   Edition_Language    :" + Edition_Language
+            print "   description    :" + description
+            print "--------------------图书字段信息-------------------\n"
 
 
 
