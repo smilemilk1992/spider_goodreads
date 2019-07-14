d3feae747dc1455d8a549c2a2d3088fb7924368f
jamesChen <702660780@qq.com>
2019-07-14 18:49:03 +0800

    add

--- spider_goodreads/spiders/goodreads_library.py
+++ spider_goodreads/spiders/goodreads_library.py
@@ -36,6 +36,7 @@ class LibrarySpider(scrapy.Spider):
 
 
 
+
 # https://www.goodreads.com/author/list/93621.Ellen_Jackson   作者书籍清单
     #开始种子URL
     # start_urls = ['https://www.goodreads.com/book/show/16077840-el-creador']
@@ -45,7 +46,7 @@ class LibrarySpider(scrapy.Spider):
         n=1
         page=6*(n-1)+1
         url = self.start_url.format(page)
-        yield scrapy.Request(url, callback=self.parse,cookies={'Cookie':'owcFirstDiscoveryRedir=promptUserFirstTime; owcLocRedirectSession=_nr.no_inst; JSESSIONID=F4376BE187CC125CD8877B5302F55B46; showhide-cite-link=-1c; __atuvc=1%7C28; LAC-User-Query=su%3ACamps+Juvenile+fiction.; LAC-User-FacetedQuery=; LAC-SR-Url=/search?q=su%3ACamps+Juvenile+fiction.&fq=&dblist=638&start=1; selected_check_boxes=; fm_facet_start=; fm_facet_se=; fm_facet_sd=; LAC-User-Location=94404; LAC-User-DisplayLocation=%E7%BE%8E%E5%9B%BD%E5%8A%A0%E5%88%A9%E7%A6%8F%E5%B0%BC%E4%BA%9A%E5%B7%9E%E7%A6%8F%E6%96%AF%E7%89%B9%E5%9F%8E%E9%82%AE%E6%94%BF%E7%BC%96%E7%A0%81%3A+94404; LAC-User-Geocode=37.5549479%2C-122.2710602; owc-holding-state="1& & "; TS015377c2=011e52333529e5a499b551cd574c4b3418cf7f1f751824403ebf4128720da3e8af96e2ced50985f46ce7a207649d699941712dad9cb56a24f5b0e811e7a9d91b03bb37f9dafc205aba2ddfc6a4c7a65dfbfaf7dce83b68ae8578e23e331f173c12e79edf3233124fdf09833aad300d122e58221f51bfbaf206652e93bdbef4efe5621a55f532d72f0453d09603776c490b39288fd32fbca2f65dfa2adc31bc397fc557bda6da33bd6dfcc3c1fd571a08f066e20f1bf738bd7e42eac69e3c5943d54dfc2d8a8f305ab2798cc64e574f86a2cb35f5ff'})
+        yield scrapy.Request(url, callback=self.parse,cookies={'LAC-User-Location':94404})
 
 
     def parse(self, response):
