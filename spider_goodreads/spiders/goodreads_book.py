49b957c1db9e7cc00488e23b618422638f0f0b73
jamesChen <702660780@qq.com>
2019-07-14 10:00:17 +0800

    add

--- spider_goodreads/spiders/goodreads_book.py
+++ spider_goodreads/spiders/goodreads_book.py
@@ -33,7 +33,7 @@ class MangoSpider(scrapy.Spider):
 
 # https://www.goodreads.com/author/list/93621.Ellen_Jackson   作者书籍清单
     #开始种子URL
-    start_urls = ['https://www.goodreads.com/book/show/34605845']
+    start_urls = ['https://www.goodreads.com/book/show/1733202']
 
 
     # def start_requests(self):
