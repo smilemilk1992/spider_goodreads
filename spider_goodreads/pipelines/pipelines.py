f5e10928e42fd06bd8f2d05873777a6c1f15b1a9
jamesChen <702660780@qq.com>
2019-07-14 09:39:39 +0800

    add

--- spider_goodreads/pipelines/pipelines.py
+++ spider_goodreads/pipelines/pipelines.py
@@ -5,7 +5,7 @@ import MySQLdb
 class SpiderGoodreadsPipeline(object):
     cc = '''INSERT IGNORE into soider1(title,bookUrl,authorName,authorNameUrl,Illustrator,IllustratorUrl,coverPic,Rating_details,score,ratings,reviews,bookFormat,Published_Time,genres,First_Published_Time,pages,Original_title,Literary_Awards,ISBN,ISBN13,Edition_Language,description)value(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
     conn = MySQLdb.connect(
-        host='127.0.0.1',
+        host='120.27.218.142',
         port=3306,
         user='worker',
         passwd='worker',
