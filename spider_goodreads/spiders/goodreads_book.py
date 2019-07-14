2a0f095d28dce47e890b53930e86e414a73cd051
jamesChen <702660780@qq.com>
2019-07-14 11:28:19 +0800

    add

--- spider_goodreads/spiders/goodreads_book.py
+++ spider_goodreads/spiders/goodreads_book.py
@@ -41,7 +41,7 @@ class MangoSpider(scrapy.Spider):
         with open('url.txt', "r") as f:
             url = f.readlines()
             for x in url:
-                yield scrapy.Request("https://www.goodreads.com/book/show/30008738-duck-goose", callback=self.parse,dont_filter=false)
+                yield scrapy.Request("https://www.goodreads.com/book/show/30008738-duck-goose", callback=self.parse,dont_filter=False)
 
 
     def parse(self, response):
