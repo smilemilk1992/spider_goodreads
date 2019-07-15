92a213fb34e0374d768d126920f91947727f19b3
jamesChen <702660780@qq.com>
2019-07-15 23:43:08 +0800

    add

--- spider_goodreads/spiders/goodreads_book.py
+++ spider_goodreads/spiders/goodreads_book.py
@@ -164,11 +164,10 @@ class MangoSpider(scrapy.Spider):
             else:
                 genres = "None"
 
-        infos = response.xpath("//ul[@class='buyButtonBar left']/li")
-        print infos
-        AmazonUrl=etree.fromstring(infos[1]).xpath("./a/@href")[0].strip()
+
+        AmazonUrl=response.xpath("//ul[@class='buyButtonBar left']/li/a[@class='buttonBar']/@href").extract()[0].strip()
         info = {}
-        storesInfo = etree.fromstring(infos[2]).xpath("//ul[@class='buyButtonBar left']/li[2]//a[@class='actionLinkLite']")
+        storesInfo = response.xpath("//div[@id='buyDropButtonStores']//a[@class='actionLinkLite']")
         for i in storesInfo:
             i = etree.fromstring(i)
             key = i.xpath("./text()")[0].strip()
