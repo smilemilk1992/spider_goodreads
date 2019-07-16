11cda769af76611d93f65f125fd5a47a004c610b
chenhao <haochen214934@sohu-inc.com>
2019-07-16 17:42:17 +0800

    add

--- spider_goodreads/spiders/goodreads_book.py
+++ spider_goodreads/spiders/goodreads_book.py
@@ -38,7 +38,7 @@ class MangoSpider(scrapy.Spider):
     logger = LogHelp.getLog("error.info")
 # https://www.goodreads.com/author/list/93621.Ellen_Jackson   作者书籍清单
     #开始种子URL
-    start_urls = ['https://www.goodreads.com/book/show/34403552-kemarley-of-anguilla']
+    start_urls = ['https://www.goodreads.com/book/show/151861.The_Big_Sleep_Over']
 
 
     # def start_requests(self):
