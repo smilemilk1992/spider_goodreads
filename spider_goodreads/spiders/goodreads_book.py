e845db4d8b9372fc6794e46e4a26ad1ecedd4b5e
chenhao <haochen214934@sohu-inc.com>
2019-07-16 17:26:32 +0800

    add

--- spider_goodreads/spiders/goodreads_book.py
+++ spider_goodreads/spiders/goodreads_book.py
@@ -38,7 +38,7 @@ class MangoSpider(scrapy.Spider):
     logger = LogHelp.getLog("error.info")
 # https://www.goodreads.com/author/list/93621.Ellen_Jackson   作者书籍清单
     #开始种子URL
-    start_urls = ['https://www.goodreads.com/book/show/34513725-the-wheels-on-the-bus---jane-cabrera---book-only']
+    start_urls = ['https://www.goodreads.com/book/show/41735400-snippets']
 
 
     # def start_requests(self):
