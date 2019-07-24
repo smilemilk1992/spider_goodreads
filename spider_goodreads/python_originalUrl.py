52bbe44abf6d93a3cc1efe997079826f38540511
jamesChen <702660780@qq.com>
2019-07-24 23:13:26 +0800

    add

--- spider_goodreads/python_originalUrl.py
+++ spider_goodreads/python_originalUrl.py
@@ -26,7 +26,7 @@ def getInfo(datas):
         key=i.get_text()
         value="https://www.goodreads.com"+i['href']
         stores[key]=value
-        goodreadsAlibrisUrl = "https://www.goodreads.com" + \
+    goodreadsAmazonUrl = "https://www.goodreads.com" + \
                          soup.find("ul", {"class": "buyButtonBar left"}).find("a", {"class": "buttonBar"})["href"]
     goodreadsAlibrisUrl=stores["Alibris"].split("&")[0]
     goodreadsWalmarteBooksUrl=stores["Walmart eBooks"].split("&")[0]
@@ -34,7 +34,7 @@ def getInfo(datas):
     goodreadsIndieBound=stores["IndieBound"].split("&")[0]
     goodreadsIndigo=stores["Indigo"].split("&")[0]
 
-    html = requests.get(goodreadsAlibrisUrl, allow_redirects=False)
+    html = requests.get(goodreadsAmazonUrl, allow_redirects=False)
 
     print link,html.headers['Location'],goodreadsAlibrisUrl,goodreadsWalmarteBooksUrl,goodreadsBarnesNoble,goodreadsIndieBound,goodreadsIndigo
 
