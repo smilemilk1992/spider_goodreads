3921cc3e0df796b0a45674d501f92ee265519cd9
jamesChen <702660780@qq.com>
2019-07-27 19:42:08 +0800

    add

--- spider_goodreads/spiders/p_news_goodreads1.py
+++ spider_goodreads/spiders/p_news_goodreads1.py
@@ -60,17 +60,20 @@ class GoodReadsSpider(scrapy.Spider):
         moreDetails=response.xpath("//div[@class='moreDetails hideDetails']").extract()
         for i in moreDetails:
             i=etree.fromstring(i)
-            dataTitle=i.xpath(".//div[@class='dataTitle']/text()")[0].strip()
-            if "ISBN" in dataTitle:
-                ISBN=i.xpath(".//div[@class='dataValue']/text()")[0].strip()
-                ISBN13=i.xpath(".//div[@class='dataValue']/span[@class='greyText']/text()")[0].strip() if i.xpath("../div[@class='dataValue']/span[@class='greyText']/text()") else None
-                xx.append([ISBN,ISBN13])
-            elif "ISBN13" in dataTitle:
-                ISBN13 = i.xpath(".//div[@class='dataValue']/text()")[0].strip()
-                ISBN=None
-                xx.append([ISBN, ISBN13])
-            else:
-                xx.append([None, None])
+            dataRow=i.xpath(".//div[@class='dataRow']")
+            for data in dataRow:
+
+                dataTitle=data.xpath("./div[@class='dataTitle']/text()")[0].strip()
+                if "ISBN" in dataTitle:
+                    ISBN=i.xpath(".//div[@class='dataValue']/text()")[0].strip()
+                    ISBN13=i.xpath(".//div[@class='dataValue']/span[@class='greyText']/text()")[0].strip() if i.xpath("../div[@class='dataValue']/span[@class='greyText']/text()") else None
+                    xx.append([ISBN,ISBN13])
+                elif "ISBN13" in dataTitle:
+                    ISBN13 = i.xpath(".//div[@class='dataValue']/text()")[0].strip()
+                    ISBN=None
+                    xx.append([ISBN, ISBN13])
+                else:
+                    xx.append([None, None])
         print response.meta["goodreadsid"],xx
 
 
