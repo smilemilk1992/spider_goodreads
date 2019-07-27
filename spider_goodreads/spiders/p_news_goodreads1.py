e87a7736546827f2c597180cbc0813931387262c
jamesChen <702660780@qq.com>
2019-07-27 18:22:39 +0800

    add

--- spider_goodreads/spiders/p_news_goodreads1.py
+++ spider_goodreads/spiders/p_news_goodreads1.py
@@ -17,7 +17,7 @@ class GoodReadsSpider(scrapy.Spider):
     '''
     name = "goodreads1"
     custom_settings = {
-        'CONCURRENT_REQUESTS': 16,  #允许的线程数
+        'CONCURRENT_REQUESTS': 8,  #允许的线程数
         'RETRY_TIMES': 3,  #重试机制
         # 'DOWNLOAD_DELAY':5,   #延时（秒）
         'ITEM_PIPELINES': {
@@ -44,14 +44,50 @@ class GoodReadsSpider(scrapy.Spider):
                 goodreadsReq=datas[1]+"."+"_".join(i for i in datas[2].split(" "))
                 title = datas[2]
                 author = datas[3]
-                yield scrapy.Request(goodreadsReq, callback=self.parse,dont_filter=False,meta={"goodreadsid":goodreadsid})
+                yield scrapy.Request(goodreadsReq, callback=self.parse,
+                                     dont_filter=False,
+                                     meta={"goodreadsid":goodreadsid,
+                                           "cudosid":cudosid,
+                                           "title":title,
+                                           "flag":False,
+                                           "isbninfo":{}})
 
 
     def parse(self, response):
-        otherEdition=response.xpath("//div[@class='otherEdition']/a/@href").extract()
-        otherEditionUrl ={}
-        if otherEdition:
-            for o in otherEdition:
-                id=re.search("book/show/(\d+)",o).group(1)
-                otherEditionUrl[id]=o
-        print response.url,otherEditionUrl
+        reqId = re.search("book/show/(\d+)",response.url).group(1)
+        bookDataBox = response.xpath(XpathRule.bookDataBox).extract()
+        infoBoxRowTitle = response.xpath(XpathRule.infoBoxRowTitle).extract()
+        if "ISBN" in infoBoxRowTitle:
+            ISBN = etree.fromstring(bookDataBox[infoBoxRowTitle.index("ISBN")]).xpath("./text()")[0].strip()
+            ISBN13 = \
+            etree.fromstring(bookDataBox[infoBoxRowTitle.index("ISBN")]).xpath(".//span[@itemprop='isbn']/text()")[
+                0].strip() if etree.fromstring(bookDataBox[infoBoxRowTitle.index("ISBN")]).xpath(
+                ".//span[@itemprop='isbn']/text()") else "None"
+        elif "ISBN13" in infoBoxRowTitle:
+            ISBN13 = etree.fromstring(bookDataBox[infoBoxRowTitle.index("ISBN13")]).xpath("./text()")[0].strip()
+            ISBN = "None"
+        else:
+            ISBN = "None"
+            ISBN13 = "None"
+        isbninfo=response.meta["isbninfo"]
+        isbninfo[reqId]=[ISBN,ISBN13]
+        if reqId is response.meta["goodreadsid"]:
+            flag=True
+            otherEdition=response.xpath("//div[@class='otherEdition']/a/@href").extract()
+            otherEditionUrl ={}
+            if otherEdition:
+                for o in otherEdition:
+                    id=re.search("book/show/(\d+)",o).group(1)
+                    otherEditionUrl[id]=o
+                    yield scrapy.Request(o, callback=self.parse, dont_filter=False, meta={"goodreadsid": response.meta["goodreadsid"],
+                                                                                             "cudosid":response.meta["cudosid"],
+                                                                                             "title":response.meta["title"],
+                                                                                          "flag":flag,
+                                                                                          "isbninfo":isbninfo})
+
+
+        if response.meta['flag']:
+            print response.meta["goodreadsid"],isbninfo
+
+
+
