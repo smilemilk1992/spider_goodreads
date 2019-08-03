442b1952639beb842ff466ad36f3ded9eb601396
jamesChen <702660780@qq.com>
2019-08-03 21:11:33 +0800

    add

--- spider_goodreads/spiders/goodreads_lib.py
+++ spider_goodreads/spiders/goodreads_lib.py
@@ -56,7 +56,7 @@ class LibrarySpider(scrapy.Spider):
         libdata="//div[@id='lib-data']"
         title = response.xpath(libdata + "//h1/text()").extract_first().strip().replace("\n", "").replace("#1", "")
         psdata = response.xpath(libdata + "//p").extract()
-        email = response.xpath(libdata + "//a[@title='发电子邮件到这图书馆']").extract()[0] if response.xpath(libdata + "//a[@title='发电子邮件到这图书馆']") else ""
+        email = response.xpath(libdata + "//a[contains(@title,'发电子邮件到这图书馆')]/text()").extract()[0] if response.xpath(libdata + "//a[contains(@title,'发电子邮件到这图书馆')]/text()") else ""
         if response.xpath(libdata+"/p[@class='lib-alias']").extract():
             p = etree.fromstring(psdata[1].replace("<br>", "\t").encode("utf-8"))
         else:
